9ff4786b51065cbbe0426c2e40e0d91505a3459e
nabi.lee <hello@nabi.kim>
2022-08-07 15:03:16 +0900

    Initial commit with nextjs-tailwindcss-template

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { GetServerSideProps, NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello NextJS!</h1>
+    </div>
+  );
+};
+
+export default Home;
