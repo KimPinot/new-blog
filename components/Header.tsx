2b34bd9a4201121c96195274e1d36968dab46fd1
nabi.lee <hello@nabi.kim>
2022-08-08 01:31:20 +0900

    Add Header

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header>
+      <div className="container pt-5 px-5 mx-auto flex items-center justify-between">
+        <Link href="/">
+          <a className="avatar w-16 h-16 btn btn-square btn-xs btn-ghost">
+            <Image src="/assets/avatar.png" alt="" layout="fill" />
+          </a>
+        </Link>
+        <div className="flex">
+          <Link href="/">
+            <a className="btn btn-ghost btn-xs">
+              <span className="text-md hover:text-primary transition-[color]">Posts</span>
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className="btn btn-ghost btn-xs">
+              <span className="text-md hover:text-primary transition-[color]">About</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "styles/taiwindcss.css";
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import AlertContextProvider from "contexts/AlertContext";
+import { Header } from "components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AlertContextProvider>
+      <Header />
       <Component {...pageProps} />
     </AlertContextProvider>
   );
