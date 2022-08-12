277267851415cee9cb84043b84c613209ebb40e3
nabi.lee <hello@nabi.kim>
2022-08-12 16:05:18 +0900

    Fix tag behavor

--- components/RenderMDX.tsx
+++ components/RenderMDX.tsx
@@ -22,9 +22,9 @@ interface IComponents extends Omit<Partial<ComponentMap>, "pre" | "code"> {
 
 const components: IComponents = {
   h1: ({ children }) => <h1 className="markdown-h1 mt-4 text-2xl font-bold">{children}</h1>,
-  h2: ({ children }) => <h1 className="markdown-h2 mt-4 text-xl font-bold">{children}</h1>,
-  h3: ({ children }) => <h1 className="markdown-h3 mt-4 text-lg font-bold">{children}</h1>,
-  h4: ({ children }) => <h1 className="markdown-h4 mt-4 text-base font-bold">{children}</h1>,
+  h2: ({ children }) => <h2 className="markdown-h2 mt-4 text-xl font-bold">{children}</h2>,
+  h3: ({ children }) => <h3 className="markdown-h3 mt-4 text-lg font-bold">{children}</h3>,
+  h4: ({ children }) => <h4 className="markdown-h4 mt-4 text-base font-bold">{children}</h4>,
   p: ({ children }) => <p className="markdown-p leading-1">{children}</p>,
   ul: ({ children }) => <ul className="markdown-ul list-disc pl-5">{children}</ul>,
   ol: ({ children }) => <ul className="markdown-ul list-decimal pl-5">{children}</ul>,
