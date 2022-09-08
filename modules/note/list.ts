18eaf9b9dee022db5ecb28f0e43586649ea0c670
PINOT <pinot.kim@kakao.com>
2022-09-08 21:42:51 +0900

    make trim deleted extension

--- modules/note/list.ts
+++ modules/note/list.ts
@@ -2,6 +2,7 @@ import * as A from "fp-ts/Array";
 import * as F from "fp-ts/function";
 import { Dirent } from "fs";
 import { readdir } from "fs/promises";
+import { deleteMdFileExtension as trim } from "modules/utils/file";
 import { resolve } from "path";
 
 const readDir = (path: string) => readdir(resolve(path), { withFileTypes: true });
@@ -9,11 +10,11 @@ const readDir = (path: string) => readdir(resolve(path), { withFileTypes: true }
 export async function noteFiles(path: string): Promise<any> {
   const dir = await readDir(path);
   const format = async (d: Dirent) =>
-    d.isDirectory() ? { [d.name]: await noteFiles(`${path}/${d.name}`) } : { [d.name]: "file" };
+    d.isDirectory() ? { [d.name]: await noteFiles(`${path}/${d.name}`) } : { [trim(d.name)]: "file" };
   return !dir.find((i) => i.isDirectory())
     ? F.pipe(
         dir,
-        A.reduce({}, (acc, cur) => ({ ...acc, [cur.name]: "file" })),
+        A.reduce({}, (acc, cur) => ({ ...acc, [trim(cur.name)]: "file" })),
       )
     : F.pipe(
         await Promise.all(F.pipe(dir, A.map(format))),
