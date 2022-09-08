af8ef0c242dc111bc3b75d0cfbf69c64f6b33edd
PINOT <pinot.kim@kakao.com>
2022-09-08 17:48:32 +0900

    Add test note

---
+++ modules/note/list.ts
@@ -0,0 +1,22 @@
+import * as A from "fp-ts/Array";
+import * as F from "fp-ts/function";
+import { Dirent } from "fs";
+import { readdir } from "fs/promises";
+import { resolve } from "path";
+
+const readDir = (path: string) => readdir(resolve(path), { withFileTypes: true });
+
+export async function noteFiles(path: string): Promise<any> {
+  const dir = await readDir(path);
+  const format = async (d: Dirent) =>
+    d.isDirectory() ? { [d.name]: await noteFiles(`${path}/${d.name}`) } : { [d.name]: "file" };
+  return !dir.find((i) => i.isDirectory())
+    ? F.pipe(
+        dir,
+        A.reduce({}, (acc, cur) => ({ ...acc, [cur.name]: "file" })),
+      )
+    : F.pipe(
+        await Promise.all(F.pipe(dir, A.map(format))),
+        A.reduce({}, (acc, cur) => ({ ...acc, ...cur })),
+      );
+}
---
+++ notes/page1/page1-1/test4.md
@@ -0,0 +1,4 @@
+---
+title: Test3
+date: Test111
+---
