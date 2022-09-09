7184875aa3b2c83f05ca5c95f306d1287805a252
PINOT <pinot.kim@kakao.com>
2022-09-09 18:25:10 +0900

    Make simple classname

--- components/layout/NoteLayout.tsx
+++ components/layout/NoteLayout.tsx
@@ -14,17 +14,17 @@ export type NoteLayoutProps = {
 
 export function NoteLayout({ notes, metadata, children }: NoteLayoutProps) {
   return (
-    <main className="max-w-5xl mx-auto flex py-0">
+    <main className="max-w-5xl mx-auto flex py-0 h-[calc(100vh-80px)]">
       <Head>
         <title>{`nabi.kim | ${metadata.title}`}</title>
         <meta property="og:title" content={`nabi.kim | ${metadata.title}`} />
         <meta property="og:description" content={metadata.description ?? "평범한 기술 블로그"} />
         <meta property="og:image" content={metadata.thumbnail ?? "https://nabi.kim/assets/opengraph.png"} />
       </Head>
-      <nav className="hidden md:block w-[250px] h-[calc(100vh-80px)] border-r-2 overflow-y-auto py-8 px-2">
+      <nav className="hidden md:block w-[250px] border-r-2 overflow-y-auto py-8 px-2">
         <Lists notes={notes} />
       </nav>
-      <div className="h-[calc(100vh-80px)] overflow-y-auto">
+      <div className="overflow-y-auto">
         <header>
           <div className="page-content">
             <h1 className="text-2xl font-bold">{metadata.title}</h1>
@@ -33,7 +33,7 @@ export function NoteLayout({ notes, metadata, children }: NoteLayoutProps) {
             </h3>
           </div>
         </header>
-        <article className={"markdown-content md:w-[calc(64rem-250px)] w-full"}>
+        <article className={"markdown-content w-full"}>
           <div className="page-content pt-0 pb-36">{children}</div>
         </article>
       </div>
