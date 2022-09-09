18e281d8dfb76b87f6d327dbc0a81411c1b8051f
PINOT <pinot.kim@kakao.com>
2022-09-09 13:22:50 +0900

    Fix content overflow

--- components/layout/NoteLayout.tsx
+++ components/layout/NoteLayout.tsx
@@ -12,10 +12,12 @@ export type NoteLayoutProps = {
 export function NoteLayout({ notes, metadata, ...props }: NoteLayoutProps) {
   return (
     <main className="max-w-5xl mx-auto flex py-0">
-      <nav className="hidden md:block min-w-[250px] min-h-[calc(100vh-80px)] border-r-2 h-full overflow-y-auto py-8 px-2">
+      <nav className="hidden md:block min-w-[250px] h-[calc(100vh-80px)] border-r-2 overflow-y-auto py-8 px-2">
         <Lists notes={notes} />
       </nav>
-      <ArticleLayout className="md:w-[calc(64rem-250px)] w-full" {...metadata} {...props} layout="note" />
+      <div className="h-[calc(100vh-80px)] overflow-y-auto">
+        <ArticleLayout className="md:w-[calc(64rem-250px)] w-full" {...metadata} {...props} layout="note" />
+      </div>
     </main>
   );
 }
