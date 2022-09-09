b1a8154e50c60f8580f92163a5710efc0fd26d2a
PINOT <pinot.kim@kakao.com>
2022-09-09 12:54:52 +0900

    Add responsive layout for Note

--- components/layout/NoteLayout.tsx
+++ components/layout/NoteLayout.tsx
@@ -12,10 +12,10 @@ export type NoteLayoutProps = {
 export function NoteLayout({ notes, metadata, ...props }: NoteLayoutProps) {
   return (
     <main className="page-content mx-auto flex py-0">
-      <nav className="min-w-[250px] min-h-[calc(100vh-80px)] border-r-2 h-full overflow-y-auto py-8 px-2">
+      <nav className="hidden md:block min-w-[250px] min-h-[calc(100vh-80px)] border-r-2 h-full overflow-y-auto py-8 px-2">
         <Lists notes={notes} />
       </nav>
-      <ArticleLayout className="w-full" {...metadata} {...props} layout="note" />
+      <ArticleLayout className="md:w-[calc(64rem-250px)] w-full" {...metadata} {...props} layout="note" />
     </main>
   );
 }
